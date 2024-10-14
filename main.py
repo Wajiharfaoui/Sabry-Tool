ce8009e5107d07dd9a62a8e2402daef8db0c7f0a
Wajih Arfaoui <arf.wajih@gmail.com>
2024-10-14 15:19:59 +0200

    add download excel

--- main.py
+++ main.py
@@ -349,7 +349,7 @@ def main():
         # Input for month and year selection
         col1, col2 = st.columns(2)
         month = col1.selectbox("Select Month", list(range(1, 13)), index=5, key="month_select")  # Default is June (month 6)
-        year = col2.selectbox("Select Year", list(range(2024, 2025)), index=1, key="year_select")  # Default is 2020
+        year = col2.selectbox("Select Year", list(range(2024, 2025)), index=0, key="year_select")  # Default is 2020
 
         if st.button("Get SEO Data", key="get_seo_data"):
             if api_id and secret_key and domain:
@@ -450,7 +450,7 @@ def main():
         st.subheader("SEA Data")
         col1, col2 = st.columns(2)
         month = col1.selectbox("Select Month", list(range(1, 13)), index=5, key="sea_month_select")
-        year = col2.selectbox("Select Year", list(range(2024, 2025)), index=1, key="sea_year_select")
+        year = col2.selectbox("Select Year", list(range(2024, 2025)), index=0, key="sea_year_select")
 
         # Dictionary to store SEA data for each domain
         sea_domains_data = {}
